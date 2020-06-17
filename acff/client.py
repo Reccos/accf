3d0b180c6bf966d049c87b9910d25ae3f08a4ee6
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2020-06-17 15:13:36 +0300

    Update client.py

--- acff/client.py
+++ acff/client.py
@@ -1,5 +1,4 @@
-import requests, json
-
+import requests
 
 class Acff(object):
 
@@ -7,25 +6,25 @@ class Acff(object):
         self.sha256Hash = sha256Hash
         self.version = 1
         self.url = 'https://datalake-prod2018.rbfa.be/graphql?'
+        self.extensions = 'extensions={"persistedQuery":{"version":%s,"sha256Hash":"%s"}}' % (self.version, self.sha256Hash)
 
-    def operation_name(self, variables={}):
-        extensions = {
-            'persistedQuery': {
-                'version': self.version,
-                'sha256Hash': self.sha256Hash
-            }  
-        }
-        url = '{}operationName=getClubInfo&variables={}&extensions={}'.format(self.url, variables, extensions)
-        result = requests.get(url)
+    def get_club_teams(self, club_id=0, language='fr'):
+        url = '{}'.format(self.url)
+        result = requests.get('https://datalake-prod2018.rbfa.be/graphql?operationName=getClubTeams&variables={"clubId":"%s","language":"%s"}&%s' % (club_id, 
+                                                                                                                                                     language,
+                                                                                                                                                     self.extensions))
         return result.json()
 
-    def get_legal_navigation_for_channel(self, variables={}):
-        extensions = {
-            'persistedQuery': {
-                'version': self.version,
-                'sha256Hash': self.sha256Hash
-            }  
-        }
-        url = '{}operationName=GetLegalNavigationForChannel&variables={}&extensions={}'.format(self.url, variables, extensions)
-        result = requests.get(url)
+    def get_club_info(self, club_id=0, language='fr'):
+        url = '{}'.format(self.url)
+        result = requests.get('https://datalake-prod2018.rbfa.be/graphql?operationName=getClubInfo&variables={"clubId":"%s","language":"%s"}&%s' % (club_id, 
+                                                                                                                                                    language,
+                                                                                                                                                    self.extensions))
+        return result.json()
+
+    def get_club(self, club_id=0, language='fr'):
+        url = '{}'.format(self.url)
+        result = requests.get('https://datalake-prod2018.rbfa.be/graphql?operationName=getClub&variables={"clubId":"%s","language":"%s"}&%s' % (club_id, 
+                                                                                                                                                language,
+                                                                                                                                                 self.extensions))
         return result.json()
